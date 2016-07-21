aa5afabc930c8e652352e894ac92e5cc5606a35d
Jonathan Birkholz <rookieone@gmail.com>
2016-07-21 10:04:54 -0500

    setup react with webpack

--- react_challenge/src/index.js
+++ react_challenge/src/index.js
@@ -1,1 +1,6 @@
-console.log('hello from webpack')
+import React from 'react'
+import { render } from 'react-dom'
+
+import Counter from './components/counters'
+
+render(<Counter />, document.getElementById('app'))

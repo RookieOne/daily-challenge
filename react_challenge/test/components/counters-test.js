019158a2771359441ce95a2c03b8b333ff805762
Jonathan Birkholz <rookieone@gmail.com>
2016-07-21 20:55:00 -0500

    refactored counter tests

--- react_challenge/test/components/counters-test.js
+++ react_challenge/test/components/counters-test.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Counters, { Counter } from 'components/counters'
 import AddCounter from 'components/add-counter'
 import { ReactTestHelper } from 'test-helpers/react'
-import { getDisplayedCounts } from 'test-helpers/counters'
+import { startingCounters, getCounterCount, getDisplayedCountsFor, getStoreCountsFor } from 'test-helpers/counters'
 import _ from 'lodash'
 
 describe('<Counters />', function () {
@@ -14,92 +14,65 @@ describe('<Counters />', function () {
   })
 
   describe('displaying counters', () => {
-    let counters = []
-    let displayedCount = {}
     beforeEach(() => {
-      const state = {
-        counters: {
-          counters: [
-            {id: 1, name: 'Cats', count: 2},
-            {id: 2, name: 'Dogs', count: 4}
-          ]
-        }
-      }
+      const state = startingCounters([
+        {id: 1, name: 'Cats', count: 2},
+        {id: 2, name: 'Dogs', count: 4}
+      ])
       this.render(<Counters />, state)
-
-      const $ = this.renderHTML()
-      counters = $('#counters li')
-      displayedCount = getDisplayedCounts(counters)
     })
-    it ('should have correct number of counters', () => {
-      assert.equal(2, counters.length)
+    it('should have correct number of counters', () => {
+      assert.equal(2, getCounterCount(this))
     })
-    it ('should have show count for Dogs counter', () => {
-      assert.equal(4, displayedCount['Dogs'])
+    it('should have show count for Dogs counter', () => {
+      assert.equal(4, getDisplayedCountsFor(this, 'Dogs'))
     })
-    it ('should have show count for Cats counter', () => {
-      assert.equal(2, displayedCount['Cats'])
+    it('should have show count for Cats counter', () => {
+      assert.equal(2, getDisplayedCountsFor(this, 'Cats'))
     })
   })
 
   describe('adding counter', () => {
-    let counters = []
     beforeEach(() => {
       this.render(<Counters />)
       const addCounter = this.findComponent(AddCounter)
       this.setValue(addCounter, 'new-counter-name', 'Greyhounds')
       this.clickButton(addCounter, 'add-counter-btn')
-
-      const $ = this.renderHTML()
-      counters = $('#counters li')
     })
-    it ('should add a counter', () => {
-      assert.equal(1, counters.length)
+    it('should add a counter', () => {
+      assert.equal(1, getCounterCount(this))
     })
-    it ('should show count for Greyhounds counter', () => {
-      const displayedCounts = getDisplayedCounts(counters)
-      assert.equal(0, displayedCounts['Greyhounds'])
+    it('should show count for Greyhounds counter', () => {
+      assert.equal(0, getDisplayedCountsFor(this, 'Greyhounds'))
     })
-    it ('should update counters in state', () => {
-      const state = this.getState()
-      const greyhoundCounter = _.find(state.counters.counters, { name: 'Greyhounds' })
-      assert(greyhoundCounter !== undefined)
-      assert.equal(0, greyhoundCounter.count)
+    it('should update counters in state', () => {
+      assert.equal(0, getStoreCountsFor(this, 'Greyhounds'))
     })
   })
 
   describe('incrementing counter', () => {
-    let counters = []
     beforeEach(() => {
-      const state = {
-        counters: {
-          counters: [
-            {id: 1, name: 'Cats', count: 2},
-            {id: 2, name: 'Dogs', count: 4}
-          ]
-        }
-      }
+      const state = startingCounters([
+        {id: 1, name: 'Cats', count: 2},
+        {id: 2, name: 'Dogs', count: 4}
+      ])
       this.render(<Counters />, state)
-      
+
       const components = this.findComponents(Counter)
       const dog = _.findWhere(components, { props: { counter: { name: 'Dogs' } } })
       this.clickButton(dog, 'increment-btn')
-      
-      const $ = this.renderHTML()
-      counters = $('#counters li')
     })
-    it ('should keep same number of counters', () => {
-      assert.equal(2, counters.length)
+    it('should keep same number of counters', () => {
+      assert.equal(2, getCounterCount(this))
     })
-    it ('should show count for Greyhounds counter', () => {
-      const displayedCounts = getDisplayedCounts(counters)
-      assert.equal(5, displayedCounts['Dogs'])
+    it('should increment count for Dogs counter', () => {
+      assert.equal(5, getDisplayedCountsFor(this, 'Dogs'))
     })
-    it ('should update counters in state', () => {
-      const state = this.getState()
-      const counter = _.find(state.counters.counters, { name: 'Dogs' })
-      assert(counter !== undefined)
-      assert.equal(5, counter.count)
+    it('should keep count for Cats counter', () => {
+      assert.equal(2, getDisplayedCountsFor(this, 'Cats'))
+    })
+    it('should update counters in state', () => {
+      assert.equal(5, getStoreCountsFor(this, 'Dogs'))
     })
   })
 }.bind(new ReactTestHelper()))
--- react_challenge/test/test-helpers/counters.js
+++ react_challenge/test/test-helpers/counters.js
@@ -1,11 +1,24 @@
-import $ from 'cheerio'
+import _ from 'lodash'
 
-export function getDisplayedCounts (counters) {
-  let result = {}
-  counters.each((i, elem) => {
-    let counterName = $(elem).find('.counter-name').text()
-    let counterCount = parseInt($(elem).find('.counter-count').text())
-    result[counterName] = counterCount
+export function startingCounters (counters) {
+  return { counters: { counters } }
+}
+export function getCounterCount (test) {
+  const $ = test.renderHTML()
+  return $('#counters li').length
+}
+
+export function getDisplayedCountsFor (test, counterName) {
+  const $ = test.renderHTML()
+  const counters = $('#counters li')
+  const elem = _.find(counters, (elem) => {
+    return $(elem).find('.counter-name').text() === counterName
   })
-  return result
+  return parseInt($(elem).find('.counter-count').text())
+}
+
+export function getStoreCountsFor (test, name) {
+  const state = test.getState()
+  const counter = _.find(state.counters.counters, { name })
+  return counter.count
 }
